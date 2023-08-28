694b8e8fa1cf44afd2544727ba74b766520bc274
yash <yt44151@gmail.com>
2023-08-28 21:16:13 +0530

    updated Homescreen

--- app/index.js
+++ app/index.js
@@ -10,11 +10,34 @@ const Home = () => {
     return(
         <SafeAreaView style= {{flex:1, backgroundColor: COLORS.lightWhite}}>
             <Stack.Screen
+                
+                // setting up the navigation bar or header with different properties
                 options={{
                     headerStyle: {backgroundColor:COLORS.lightWhite},
                     headerShadowVisible: false, // using this to ignore the shadow of the title page line.
+                    // additional property header left: that is an arrow function
+                    headerLeft: () => (
+                        <ScreenHeaderBtn  iconUrl= {icons.menu} dimension='60%'/>
+                    ),
+                    headerRight: () => (
+                        <ScreenHeaderBtn  iconUrl= {icons.profile} dimension='100%'/>
+                    ),
+                    headerTitle: " "
                 }}
-            />
+                />
+                {/* //adding below nav bar */}
+                <ScrollView showsVerticalScrollIndicator={false}>  
+                    <View style= {{flex:1, padding: SIZES.medium}}>
+                        {/* components */}
+                        <Welcome
+                        
+                        />
+                        <Nearbyjobs/>
+                        <Popularjobs/>
+                        
+                    </View>
+
+                </ScrollView>
         </SafeAreaView>
     )
 }
