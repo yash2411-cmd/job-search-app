d2678a9d7ad97cf0079433242fe1b17c10d034b3
yash <yt44151@gmail.com>
2023-09-05 19:15:25 +0530

    updated jobs details page

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, TouchableOpacity, Image, FlatList } from 'react-native'
+import { SIZES } from '../../../constants'
 import styles from './tabs.style'
 
-const Tabs = () => {
+
+const TabButton = ({name, activeTab, onHandleSearchType}) => {
   return (
-    <View>
-      <Text>Tabs</Text>
+    <TouchableOpacity
+      style={styles.btn(name, activeTab)}
+      onPress={onHandleSearchType}
+    >
+      <Text style={styles.btnText(name, activeTab)}>{name}</Text>
+    </TouchableOpacity>
+  );
+}
+
+
+const Tabs = ({tabs, activeTab, setactiveTab}) => {
+  return (
+    <View style= {styles.container}>
+      <FlatList
+      data = {tabs}
+      renderItem={({item}) => (
+        <TabButton
+          name={item}
+          activeTabs={activeTab}
+          onHandleSearchType={()=> setactiveTab(item)}
+          />
+      )}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      keyExtractor={item => item}
+      contentContainerStyle= {{columnGap: SIZES.small /2}}
+      />
     </View>
   )
 }
